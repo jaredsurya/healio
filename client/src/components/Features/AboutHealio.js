ac02667435ac28bdc5cf7e96c753af6be5351344
Jared Dietz <jaredsurya@gmail.com>
2024-01-10 13:10:36 -0500

    edited about section

--- client/src/components/Features/AboutHealio.js
+++ client/src/components/Features/AboutHealio.js
@@ -7,15 +7,15 @@ function AboutHealio() {
       <Typography variant="h3" gutterBottom align="center">About Healio</Typography>
       <Box maxWidth={"85%"} margin="auto">
       <Typography variant="h6" align="center" >
-        Healio was created to be a means for the healer community to better
-        engage with the needing populace. Its intent at first was to be a guide
-        to new healing modalities which visitors may have never seen. You'll
-        find that on the services page of your liking you can see the associated
-        healers on this site which offer that kind of service.
+        Healio was created to be a means for healing practicioners to find and connect
+        to the needful populace. Its intent is also to be a guide to
+         healing modalities which page visitors may have never seen. You'll
+        find that on any chosen services page you can see the associated
+        healers on this site who offer that kind of service.
       </Typography>
       <br/>
       <Typography variant="h6" align="center">If there is any issue you would like to report or if you would
-        just like to inquire about Healio, please contact Jared at <strong>jaredsurya@gmail.com</strong>
+        like to inquire about Healio, please contact Jared at <strong>jaredsurya@gmail.com</strong>
       </Typography>
       </Box>
     </Box>
